cad5df54c1df58a7f622380dc2d63cdd8d5ab7d2
Pineapples27 <warren.newland27@gmail.com>
2023-02-13 11:01:36 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func Save(key string, contents interface{}) error {
+	jsonBytes, err := json.Marshal(contents)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(keyToFileName(key), jsonBytes, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Fetch(key string) (returnedContents interface{}, err error) {
+	bytes, err := ioutil.ReadFile(keyToFileName(key))
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(bytes, &returnedContents)
+	if err != nil {
+		return nil, err
+	}
+	return
+}
+
+func Release(key string) error {
+	err := os.Remove(keyToFileName(key))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func keyToFileName(key string) string {
+	if !strings.Contains(key, ".json") {
+		key = fmt.Sprintf("%s.json", key)
+	}
+	return key
+}
